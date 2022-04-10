5ea6240ae27703579219ab3f9696ae00de1a473f
Mark J Rawlins <mark.jeffrey.rawlins@gmail.com>
2022-04-10 21:25:52 +0900

    switch service worker from index.js to index.html

--- src/client/index.js
+++ src/client/index.js
@@ -4,16 +4,6 @@ import formHandler from './js/formHandler.js'
 //we need to import our main.scss file
 import './styles/main.scss'
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js').then(registration => {
-            console.log('SW registered: ', registration);
-        }).catch(registrationError => {
-            console.log('SW registration failed: ', registrationError);
-        });
-    });
-}
-
 //Run the formHandler function
 window.addEventListener('DOMContentLoaded', () => {
     const submit = document.getElementById('submit');
