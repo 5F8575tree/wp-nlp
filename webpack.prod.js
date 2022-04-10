717ab9dce3bec295e92cb3f9bfac54fe5e4cd397
Mark J Rawlins <mark.jeffrey.rawlins@gmail.com>
2022-04-10 13:30:13 +0900

    Install workbox-webpack-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 const Dotenv = require('dotenv-webpack')
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 
 module.exports = {
     mode: "production",
@@ -50,6 +52,7 @@ module.exports = {
         ),
         new MiniCssExtractPlugin(
 
-        )
+        ),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
